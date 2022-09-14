a901b044b644a4070189c365e1381a270291f46e
Tuan <vuanhtuanthon2@gmail.com>
2022-09-15 02:02:14 +0700

    fix commit

--- src/redux/action/QuanLyZodiacAction.js
+++ src/redux/action/QuanLyZodiacAction.js
@@ -1,70 +1,69 @@
-import axios from "axios"
-
+import axios from 'axios'
 
 export const QuanLyZodiacAction = () => {
-    return async (dispatch) => {
-        try {
-            let result = await axios({
-                url: 'https://www.psychologicalcounselingv1.somee.com/api/Zodiacs',
-                method: 'GET'
-            })
-            const action = {
-                type: 'LAY_DANH_SACH_ZODIAC',
-                arrZodiac: result.data
-            }
-            dispatch(action);
-        } catch (error) {
-            console.log(error);
-        }
+  return async dispatch => {
+    try {
+      let result = await axios({
+        url: 'https://www.psychologicalcounselingv1.somee.com/api/Zodiacs',
+        method: 'GET'
+      })
+      const action = {
+        type: 'LAY_DANH_SACH_ZODIAC',
+        arrZodiac: result.data
+      }
+      dispatch(action)
+    } catch (error) {
+      console.log(error)
     }
+  }
 }
 
-
-export const DetailZodiacAction = (id) => {
-    return async (dispatch) => {
-        try {
-            let result = await axios({
-                url: 'http://www.psychologicalcounselingv1.somee.com/api/Zodiacs/' + id,
-                method: 'GET'
-            })
-            const action = {
-                type: 'CHI_TIET_ZODIAC',
-                chiTietZodiac: result.data
-            }
-            dispatch(action)
-        } catch (error) {
-            console.log(error);
-        }
+export const DetailZodiacAction = id => {
+  return async dispatch => {
+    try {
+      let result = await axios({
+        url: 'http://www.psychologicalcounselingv1.somee.com/api/Zodiacs/' + id,
+        method: 'GET'
+      })
+      const action = {
+        type: 'CHI_TIET_ZODIAC',
+        chiTietZodiac: result.data
+      }
+      dispatch(action)
+    } catch (error) {
+      console.log(error)
     }
+  }
 }
 export const EditZodiacAction = (id, formValue) => {
-    return async (dispatch) => {
-        try {
-            let result = await axios({
-                url: 'http://www.psychologicalcounselingv1.somee.com/api/Zodiacs/' + id, formValue,
-                method: 'PUT'
-            })
+  return async dispatch => {
+    try {
+      let result = await axios({
+        url: 'http://www.psychologicalcounselingv1.somee.com/api/Zodiacs/' + id,
+        formValue,
+        method: 'PUT'
+      })
 
-            console.log(result);
-        } catch (error) {
-            console.log(error);
-        }
+      console.log(result)
+    } catch (error) {
+      console.log(error)
     }
+  }
 }
 export const LayDanhSachHousesAction = () => {
-    return async (dispatch) => {
-        try {
-            let result = await axios({
-                url: 'https://www.psychologicalcounselingv1.somee.com/api/Houses',
-                method: 'GET'
-            })
-            const action = {
-                type: 'LAY_DANH_SACH_HOUSES',
-                arrHouses: result.data
-            }
-            dispatch(action)
-        } catch (error) {
-            console.log(error);
-        }
+  return async dispatch => {
+    try {
+      let result = await axios({
+        url: 'https://www.psychologicalcounselingv1.somee.com/api/Houses',
+        method: 'GET'
+      })
+      const action = {
+        type: 'LAY_DANH_SACH_HOUSES',
+        arrHouses: result.data
+      }
+      dispatch(action)
+    } catch (error) {
+      console.log(error)
     }
+  }
 }
